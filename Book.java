db0f098c95dd2fd0bccd2f07e233735172cda44f
ilaya27464 <127576283+ilaya27464@users.noreply.github.com>
2023-03-26 15:08:50 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -22,3 +22,10 @@ public class Main1{
         System.out.println("The title is: "+m.getTitle());
     }
 }
+
+Output:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=5162:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java full stack\out\production\java full stack" Main1
+The title is: The Alchemist
+
+Process finished with exit code 0
