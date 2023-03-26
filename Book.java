127461a1a1ed3195dce1ebdf84256d92aafbd02f
ilaya27464 <127576283+ilaya27464@users.noreply.github.com>
2023-03-26 15:06:38 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -5,3 +5,20 @@ abstract class Book{
         return title;
     }
 }
+
+class MyBook extends Book{
+    @Override
+    void setTitle(String s){
+        title = s;
+    }
+}
+
+Main Function:
+
+public class Main1{
+    public static void main(String[] args){
+        MyBook m= new MyBook();
+        m.setTitle("The Alchemist");
+        System.out.println("The title is: "+m.getTitle());
+    }
+}
