933a240c7c0ea85647b114e0fc1e13245c63604f
ilaya27464 <127576283+ilaya27464@users.noreply.github.com>
2023-03-26 15:32:23 +0530

    Update Exceptions.java

--- Exceptions.java
+++ Exceptions.java
@@ -11,7 +11,16 @@ public class Exceptions {
             int result = a / b;
             System.out.println("Result = " + result);
         } catch (Exception e) {
-            System.out.println("Exception: " + e);
+            System.out.println( e);
         }
     }
 }
+
+Output:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=7243:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java full stack\out\production\java full stack" Exceptions
+Enter two integers: 
+hello
+java.util.InputMismatchException
+
+Process finished with exit code 0
