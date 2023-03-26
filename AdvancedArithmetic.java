0f1009f61793449656405c99232fb141d719f740
ilaya27464 <127576283+ilaya27464@users.noreply.github.com>
2023-03-26 15:19:34 +0530

    Update AdvancedArithmetic.java

--- AdvancedArithmetic.java
+++ AdvancedArithmetic.java
@@ -25,6 +25,7 @@ Main Function:
 public class Math {
     public static void main(String[] args) {
         MyCalculator m=new MyCalculator();
+        System.out.println("I implemented: AdvancedArithmetic");
         System.out.println(""+m.divisor_sum(10001));
 
     }
@@ -34,6 +35,8 @@ public class Math {
 Output:
 
 C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=6142:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java full stack\out\production\java full stack" Math
+
+I implemented: AdvancedArithmetic
 72
 
 Process finished with exit code 0
